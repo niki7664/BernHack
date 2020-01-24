cd7cc6f155adf6a91cce4e4b7eb9260d60579839
niki7664 <niki.kstha7664@gmail.com>
2020-01-24 14:52:12 +0545

    .....

---
+++ lib/demo_data.dart
@@ -0,0 +1,66 @@
+class LocationInfo{
+    
+    String title;
+    List<String> image;
+    String description;
+
+    LocationInfo({this.title, this.image, this.description})
+
+}
+
+class DemoData{
+
+    List<LocationInfo> picnicSpots = [
+        LocationInfo(
+            title : "Balaju water garden",
+            description :"Situated five kilometers north-west of Kathmandu, Balaju Water Garden is an ideal place for rest and relaxation. The park features a long line of twenty-two stone water spouts from the mid-eighteenth century, each of which is ornately carved with crocodile heads. The garden also includes many other ponds, some of them containing large and small varieties of fish. Adjoining the garden is an Olympic-size swimming pool open to the public. Balaju Industrial Estate is located nearby.",
+        ),
+
+        LocationInfo(
+            title : "Kakani",
+            description : "Kakani is in the Nuwakot District. It is 20 km along the road from Kathmandu to Trisuli Baazar, the turn-off to the village is just before the police checkpoint where there is a cluster of shops. The road to the accommodation options in the village passes though a UN AFP training camp.Most of the towns around Kathmandu sit at the bottom of the valley – you have to travel to the valley rim to really get decent views of the Himalaya. Set atop a ridge at 2073m, just off the road to Trisuli Bazaar, Kakani is the quieter, slower cousin of Dhulikhel and Nagarkot. From a series of high points along the ridge, there are magnificent views of the Himalayan skyline stretching all the way from Annapurna to Everest, via Manaslu, Ganesh Himal, Gauri Shankar, Dorje Lekpa and Shishapangma.Kakani is a major gateway for Picnic, hiking and mountain biking routes in neighbouring Shivapuri National Park.",
+        ),
+
+        LocationInfo(
+            title : "Manjushree park",
+            description : "Located in the Chobhar gorge of the Kirtipur Municipality, Manjushree Park should be on your list of places to visit with family in Kirtipur, Nepal. Located near the Manjushree cave, this park is ideal for sightseeing and photography. Located on a small hill, this park offers visitors with panoramic views of the beautiful surroundings. The park is believed to be named after Manjushree, who was a Chinese saint. Do not give this park when you are in Nepal.",
+
+        ),
+
+        LocationInfo( 
+            title : "Mini Ilam Tea garden",
+            description : "Bhotechaur is well known for the tea garden (known as Everest Tea Garden) located in Hibung Village in Melamchi Municipality , Everest tea garden which is also popular as “Mini Ilam” is one of the beautiful places near Kathmandu for short duration outing. Mini Ilam is accessible by hiking route of 3 km from Prakriti Resort. Everest Tea Garden used to be a trending traveling destination of the people of Kathmandu valley for Picnic, photo shoot, refreshment visit as well as one night stays. Presently, the management of the Everest garden has closed the entry of visitors for some safety reasons of the plants itself. The visitors are allowed to view the garden only from distance apart.",
+
+
+
+        )
+
+        LocationInfo(
+            title : "Nagarkot",
+            description : "Nagarkot has a reputation as the top spot for enjoying Himalayan views from the comfort of your hotel balcony. Just 32km from Kathmandu, the village is packed with hotels lining a ridge, affording one of the broadest possible views of the Himalaya, with eight ranges visible (Annapurna, Manaslu, Ganesh Himal, Langtang, Jugal, Rolwaling, Everest and Numbur). However, timing is everything, as the mountains are notorious for disappearing behind cloudy skies. The best viewing seasons are October to December and March to April, when clear skies are likely. Other than views, the town offers very little, so if you want to get out of your hotel you'll need to head to another resort, or into the outdoors. Note that if you have just arrived at Tribhuvan Airport, it's possible to come directly to Nagarkot and chill out in nature if you wish to avoid the city. Bring warm clothing after mid-October.",
+
+        )
+
+        LocationInfo(
+            title : "Shankh park",
+            description :"Sankha Park is a park near Chappal Karkhana,The residents visit the park for picnicking, meeting and morning walk.The park is managed by Kathmandu Metropolitan City. This park is situated beside a busy ring road and offers solace to the many passers-by from the pollution and heat of the city.",
+
+
+
+        )
+
+        LocationInfo(
+            title : "Sikali",
+            description : "Sikali Temple – Khokana, is one of the most beautiful temple built around 336 Nepal Sambat. The temple with open, clean and green meadow, surrounding hills and paddy fields gives the feel of Switzerland.“Locals believe that if we don’t sacrifice the animals on different occasions, misfortunes and tragedies will occur”Sikali Temple lies in the southern part of Kathmandu Valley at an approximate distance of 10 KM. The temple is located in an isolated place, outside of the main settlements, but the surrounding is heavenly.Sikali Mai Temple is a huge Three-storey temple, where goddess Rudrayani is worshipped and is considered as the protector of the locals and localities.It is also believed that once Sikhi Buddha had come and taught Dhamma for locals.The surrounding the clean, green and romantic. Paddy fields near the temple, flowing Bagmati River and the view of Chandragiri and Champadevi hills makes the moment unbelievable.",
+        )
+
+        LocationInfo(
+            title : "Tribhuvan Park",
+            description : "Tribhuwan park is picnic spot. Which is situated at thankot of kathmandu . it is about 15 minutes drive towards west from kalanki . this park lies by the highway which makes easy access to those who want to enjoy their free time visiting the park and going there for picnic.",
+        )
+    ];
+
+
+
+    
+}
